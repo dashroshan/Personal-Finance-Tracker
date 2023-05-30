b88e66b8d6f9ded2deb951d0f1da69db530d89fa
Roshan Dash <roshan1337d@gmail.com>
2023-05-31 01:26:53 +0530

    functionalities

--- index.js
+++ index.js
@@ -1,21 +1,4 @@
-let data = {
-    remainingBudget: 5000,
-    totalExpenses: 10000,
-    items: {
-        "abcd123": {
-            date: "2023-05-30",
-            cost: 5000,
-            name: "Smart watch",
-            editMode: false
-        },
-        "abcd124": {
-            date: "2023-05-30",
-            cost: 50000,
-            name: "Laptop",
-            editMode: true
-        }
-    }
-};
+let data;
 
 const saveRemainingBudgetBtn = document.getElementById("saveRemainingBudgetBtn");
 const remainingBudget = document.getElementById("remainingBudget");
@@ -27,6 +10,8 @@ const addExpenseBtn = document.getElementById("addExpenseBtn");
 const addExpenseName = document.getElementById("addExpenseName");
 const addExpenseDate = document.getElementById("addExpenseDate");
 const addExpenseCost = document.getElementById("addExpenseCost");
+
+addExpenseDate.valueAsNumber = Date.now() - (new Date()).getTimezoneOffset() * 60000;
 
 const resetAllBtn = document.getElementById("resetAllBtn");
 
@@ -41,7 +26,16 @@ function randomString(len) {
 }
 
 saveRemainingBudgetBtn.addEventListener("click", () => {
-    data.remainingBudget = remainingBudgetInput.value;
+    remainingBudgetAmount = parseInt(remainingBudgetInput.value, 10);
+    if (remainingBudgetAmount < 0 || !remainingBudgetAmount) {
+        document.getElementById("setBudgetError").style.display = "block";
+        setTimeout(() => {
+            document.getElementById("setBudgetError").style.display = "none";
+        }, 1500);
+        return;
+    }
+
+    data.remainingBudget = remainingBudgetAmount;
     dataUpdated();
 })
 
@@ -63,7 +57,17 @@ addExpenseBtn.addEventListener("click", () => {
         editMode: false
     };
 
+    if (itemData.cost < 0 || !itemData.cost || !itemData.name) {
+        document.getElementById("setExpenseError").style.display = "block";
+        setTimeout(() => {
+            document.getElementById("setExpenseError").style.display = "none";
+        }, 1500);
+        return;
+    }
+
     data.items[randomString(15)] = itemData;
+    data.remainingBudget -= parseInt(addExpenseCost.value, 10);
+    data.remainingBudget = data.remainingBudget > 0 ? data.remainingBudget : 0;
     dataUpdated();
 })
 
@@ -72,17 +76,45 @@ const deleteItem = (id) => {
     dataUpdated();
 }
 
+const updateItem = (id) => {
+    const cost = parseInt(document.getElementById("itemValCost" + id).value, 10);
+    const name = document.getElementById("itemValName" + id).value;
+    const date = document.getElementById("itemValDate" + id).value;
+
+    const oldCost = data.items[id].cost;
+
+    data.items[id] = {
+        date: date,
+        cost: cost,
+        name: name,
+        editMode: false
+    };
+
+    data.remainingBudget -= parseInt(cost - oldCost, 10);
+    data.remainingBudget = data.remainingBudget > 0 ? data.remainingBudget : 0;
+
+    dataUpdated();
+}
+
+const openEditMode = (id) => {
+    data.items[id].editMode = true;
+    dataUpdated();
+}
+
 
 const dataUpdated = () => {
-    remainingBudget.innerText = data.remainingBudget + " Rs";
-    totalExpenses.innerText = data.totalExpenses + " Rs";
-
     let listBodyContent = "";
     let dataItemsToList = []
 
+    let totalCost = 0
     for (const [key, value] of Object.entries(data.items)) {
         dataItemsToList.push({ ...value, id: key });
+        totalCost += value.cost;
     }
+    data.totalExpenses = totalCost;
+
+    remainingBudget.innerText = data.remainingBudget + " Rs";
+    totalExpenses.innerText = data.totalExpenses + " Rs";
 
     dataItemsToList.sort((a, b) => {
         if (a.date > b.date) return -1;
@@ -91,20 +123,22 @@ const dataUpdated = () => {
     })
 
     for (let item of dataItemsToList) {
-        const costEdit = `<input type="text" value="${item.cost}">`;
+        const costEdit = `<input id="itemValCost${item.id}" type="text" value="${item.cost}">`;
         const costNonEdit = `<b>${item.cost}</b>`;
 
-        const nameEdit = `<input type="text" value="${item.name}"/>`;
+        const nameEdit = `<input id="itemValName${item.id}" type="text" value="${item.name}"/>`;
         const nameNonEdit = item.name;
 
-        const dateEdit = `<input type="date" value="${item.date}" />`;
+        const dateEdit = `<input id="itemValDate${item.id}" type="date" value="${item.date}" />`;
         const dateNonEdit = "30 May 2023";
 
+        const editSaveBtn = item.editMode ? `updateItem('${item.id}')` : `openEditMode('${item.id}')`;
+
         listBodyContent += `
-        <tr id="item${item.id}}">
+        <tr>
             <td>
                 <div class="tableBtns">
-                    <button onclick="deleteItem('${item.id}')"><img src="./assets/${item.editMode ? 'saveWhite' : 'edit'}.svg"></button>
+                    <button onclick="${editSaveBtn}"><img src="./assets/${item.editMode ? 'saveWhite' : 'edit'}.svg"></button>
                     <button onclick="deleteItem('${item.id}')"><img src="./assets/delete.svg"></button>
                 </div>
             </td>
@@ -115,6 +149,21 @@ const dataUpdated = () => {
     }
 
     listBody.innerHTML = listBodyContent;
+
+    localStorage.setItem('personalFinanceTrackerData', JSON.stringify(data));
 }
 
-dataUpdated();
+const oldData = localStorage.getItem('personalFinanceTrackerData');
+if (oldData) {
+    data = JSON.parse(oldData);
+    dataUpdated();
+}
+else {
+    data = {
+        remainingBudget: 0,
+        totalExpenses: 0,
+        items: {}
+    };
+    localStorage.setItem('personalFinanceTrackerData', JSON.stringify(data));
+    dataUpdated();
+}
