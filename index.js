8375983d41cb1131f253b507e3385e40963d619f
Roshan Dash <roshan1337d@gmail.com>
2023-05-31 02:23:16 +0530

    loader

--- index.js
+++ index.js
@@ -203,3 +203,22 @@ else {
     localStorage.setItem('personalFinanceTrackerData', JSON.stringify(data));
     dataUpdated();
 }
+
+function hideLoader() {
+    const onPageLoad = () => {
+        setTimeout(() => {
+            document.getElementById("loader_block").style.opacity = 0;
+            setTimeout(() => {
+                document.getElementById("loader_block").style.display = "none";
+            }, 310);
+        }, 200);
+    };
+    if (document.readyState === 'complete') {
+        onPageLoad();
+    } else {
+        window.addEventListener('load', onPageLoad, false);
+        return () => window.removeEventListener('load', onPageLoad);
+    }
+}
+
+hideLoader();
