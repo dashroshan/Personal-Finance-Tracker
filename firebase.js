b0e585ed26cb712011e71eb7ca7593aa013bdc6d
Roshan Dash <roshan1337d@gmail.com>
2023-05-31 11:56:51 +0530

    firebase

--- firebase.js
+++ firebase.js
@@ -1,8 +1,7 @@
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js";
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
+const googleAuthBtn = document.getElementById("googleAuthBtn");
+const googleAuthText = document.getElementById("googleAuthText");
 
-
-const firebaseConfig = {
+var firebaseConfig = {
     apiKey: "AIzaSyAcHe3XUg7sov-w1g2rcgQGQlzlFnDQA5A",
     authDomain: "personal-finance-tracker-7b8c4.firebaseapp.com",
     projectId: "personal-finance-tracker-7b8c4",
@@ -10,40 +9,66 @@ const firebaseConfig = {
     messagingSenderId: "499863185106",
     appId: "1:499863185106:web:12370a6b86116abd34af0b"
 };
-const app = initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
+let provider = new firebase.auth.GoogleAuthProvider();
 
+var db = app.firestore();
 
-const provider = new GoogleAuthProvider();
-const auth = getAuth();
+class User {
+    usersRef = db.collection("users");
 
-const googleAuthBtn = document.getElementById("googleAuthBtn");
-
-googleAuthBtn.addEventListener("click", () => {
-    signInWithPopup(auth, provider)
-        .then((result) => {
-            const credential = GoogleAuthProvider.credentialFromResult(result);
-            const token = credential.accessToken;
-            const user = result.user;
-            console.log(user);
-        }).catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            const email = error.customData.email;
-            const credential = GoogleAuthProvider.credentialFromError(error);
-        });
-})
-
-function Check() {
-    onAuthStateChanged((user) => {
-        if (user) {
-            // User Signed In
-            console.log("User Signed In!!");
-        } else {
-            // User is signed out
-            console.log("User Signed out!!");
-            // ...
+    async update(email, userData) {
+        try {
+            await this.usersRef.doc(email).set(userData);
+        } catch (error) {
+            console.error(error)
         }
-    });
+    }
+
+    async get(email) {
+        try {
+            let doc = await this.usersRef.doc(email).get();
+            if (doc.exists)
+                console.log(doc.data());
+        }
+        catch (error) {
+            console.error(error);
+        }
+    }
 }
 
-Check();
+function GoogleLogin() {
+    firebase.auth().signInWithRedirect(provider).then(res => {
+        checkAuthState();
+    }).catch(e => {
+        console.log(e)
+    })
+}
+
+function checkAuthState() {
+    firebase.auth().onAuthStateChanged(async user => {
+        if (user) {
+            googleAuthText.innerText = "Signout";
+            const userObj = new User();
+            console.log(await userObj.get(user.email));
+        } else {
+            googleAuthText.innerText = "Signin with Google";
+        }
+    })
+}
+
+function LogoutUser() {
+    firebase.auth().signOut().then(() => {
+        checkAuthState();
+    }).catch(e => {
+        console.log(e)
+    })
+}
+
+function handleAuthBtn() {
+    if (googleAuthText.innerText === "Signout") LogoutUser();
+    else GoogleLogin();
+}
+
+googleAuthBtn.addEventListener("click", handleAuthBtn);
+checkAuthState();
