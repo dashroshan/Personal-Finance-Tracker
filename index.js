fede3b89ee28771fe446f36011830f2601b31159
Roshan Dash <roshan1337d@gmail.com>
2023-05-31 12:19:45 +0530

    firestore

--- index.js
+++ index.js
@@ -1,5 +1,93 @@
 let data;
 
+const googleAuthBtn = document.getElementById("googleAuthBtn");
+const googleAuthText = document.getElementById("googleAuthText");
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAcHe3XUg7sov-w1g2rcgQGQlzlFnDQA5A",
+    authDomain: "personal-finance-tracker-7b8c4.firebaseapp.com",
+    projectId: "personal-finance-tracker-7b8c4",
+    storageBucket: "personal-finance-tracker-7b8c4.appspot.com",
+    messagingSenderId: "499863185106",
+    appId: "1:499863185106:web:12370a6b86116abd34af0b"
+};
+const app = firebase.initializeApp(firebaseConfig);
+let provider = new firebase.auth.GoogleAuthProvider();
+
+var db = app.firestore();
+let email = "";
+
+class User {
+    usersRef = db.collection("users");
+
+    async update(userData) {
+        try {
+            await this.usersRef.doc(email).set(userData);
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    async get() {
+        try {
+            let doc = await this.usersRef.doc(email).get();
+            if (doc.exists)
+                return doc.data();
+        }
+        catch (error) {
+            console.error(error);
+        }
+    }
+}
+
+function GoogleLogin() {
+    firebase.auth().signInWithRedirect(provider).then(res => {
+        checkAuthState();
+    }).catch(e => {
+        console.log(e)
+    })
+}
+
+function checkAuthState() {
+    firebase.auth().onAuthStateChanged(async user => {
+        if (user) {
+            googleAuthText.innerText = "Signout";
+            const userObj = new User();
+            email = user.email;
+            const userData = await userObj.get();
+            data = userData;
+            dataUpdated();
+        } else {
+            googleAuthText.innerText = "Signin with Google";
+            data = {
+                remainingBudget: 0,
+                totalExpenses: 0,
+                items: {}
+            };
+            dataUpdated();
+        }
+    })
+}
+
+function LogoutUser() {
+    firebase.auth().signOut().then(() => {
+        checkAuthState();
+    }).catch(e => {
+        console.log(e)
+    })
+}
+
+function handleAuthBtn() {
+    if (googleAuthText.innerText === "Signout") LogoutUser();
+    else GoogleLogin();
+}
+
+googleAuthBtn.addEventListener("click", handleAuthBtn);
+
+
+
+
+
 const saveRemainingBudgetBtn = document.getElementById("saveRemainingBudgetBtn");
 const remainingBudget = document.getElementById("remainingBudget");
 const remainingBudgetInput = document.getElementById("remainingBudgetInput");
@@ -150,7 +238,7 @@ const openEditMode = (id) => {
 }
 
 
-const dataUpdated = () => {
+const dataUpdated = async () => {
     let listBodyContent = "";
     let dataItemsToList = []
 
@@ -198,23 +286,13 @@ const dataUpdated = () => {
 
     listBody.innerHTML = listBodyContent;
 
-    localStorage.setItem('personalFinanceTrackerData', JSON.stringify(data));
+    if (email !== "") {
+        const userObj = new User();
+        await userObj.update(data);
+    }
 }
 
-const oldData = localStorage.getItem('personalFinanceTrackerData');
-if (oldData) {
-    data = JSON.parse(oldData);
-    dataUpdated();
-}
-else {
-    data = {
-        remainingBudget: 0,
-        totalExpenses: 0,
-        items: {}
-    };
-    localStorage.setItem('personalFinanceTrackerData', JSON.stringify(data));
-    dataUpdated();
-}
+checkAuthState();
 
 function hideLoader() {
     const onPageLoad = () => {
